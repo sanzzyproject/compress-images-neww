defbd4bb056422ba65d785c6f2a40a74ccc73e41
saannndec5ty <stokakunsanzz250@gmail.com>
2026-02-10 18:43:04 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; // <--- WAJIB ADA INI
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SANN404 Image Compressor",
-  description: "Aplikasi kompres foto online gratis, aman, dan tanpa watermark. Teknologi serverless oleh SANN404 FORUM.",
+  description: "Aplikasi kompres foto online gratis, aman, dan tanpa watermark.",
 };
 
 export default function RootLayout({
