3151d6fb740e15b00e1b23649847d881299029dd
saannndec5ty <stokakunsanzz250@gmail.com>
2026-02-10 18:06:56 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SANN404 Image Compressor",
+  description: "Aplikasi kompres foto online gratis, aman, dan tanpa watermark. Teknologi serverless oleh SANN404 FORUM.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
